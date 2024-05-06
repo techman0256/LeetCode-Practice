43f0c9173cea75fcab92e39969c654b3d8ae3b60
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-05-06 11:43:40 +0530

    Time: 86 ms (17.82%) | Memory: 44.6 MB (20.17%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,28 +2,55 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int dp[][] = new int[n + 1][n + 1];
-        for (int i=0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
+        int dp[] = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i=0; i < n; i++) {
+            for (int j=0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
         }
-        return fx(0, -1, nums, dp);
+
+        int max = dp[0];
+        System.out.print(dp[0] + " ");
+        for (int i=1; i < n; i++) {
+            max = Math.max(max, dp[i]);
+            System.out.print(dp[i] + " ");
+        }
+
+        return max;
+
+        // int dp[][] = new int[n + 1][n + 1];
+
+        // for (int i=1; i <= n; i++) {
+        //     for (int j=1; j <= i; j++) {
+
+        //     }
+        // }
+
+        // for (int i=1; i <= n; i++) {
+        //     for (int j=1; j <= n; j++) {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+
+        //     System.out.println():
+        // }
+
     }
-    static int fx(int idx, int last, int nums[], int dp[][]) {
-        if (idx == nums.length) {
-            return 0;
-        }
-
-        if (dp[idx][last + 1] != -1) {
-            return dp[idx][last + 1];
-        }
-
-        int take = 0;
-        if (last == -1 || nums[idx] > nums[last]) {
-            take = 1 + fx(idx + 1, idx, nums, dp);
-        }
+    static int LIS(int nums[], int idx, int last) {
+        // base case
+        if (idx == nums.length) return 0;
         
-        int notTake = fx(idx + 1, last, nums, dp);
+        // write a recurrunce
 
-        return dp[idx][last + 1] = Math.max(take, notTake);
+        int take = 0, notTake = 0;
+        if (last == -1 || nums[last] < nums[idx]) {
+            take = 1 + LIS(nums, idx + 1, idx);
+        }
+        notTake = LIS(nums, idx + 1, last);
+
+        return Math.max(take, notTake);
     }
 }
