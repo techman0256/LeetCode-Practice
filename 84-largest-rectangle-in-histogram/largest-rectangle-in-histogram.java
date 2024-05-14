f87fb29fc1a6a1d815829bf359c1f992063e840b
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-05-14 20:12:14 +0530

    Time: 62 ms (69.71%) | Memory: 57.9 MB (21.87%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -2,45 +2,24 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
 
-        int lse[] = new int[n];
-        int rse[] = new int[n];
+        int area = 0;
 
         Stack<Integer> stk = new Stack<Integer>();
-        
-        
-        for (int i=0; i < n; i++) {
-            while (!stk.isEmpty() && heights[i] <= heights[stk.peek()]) {
-                stk.pop();
-            }
 
-            if (stk.isEmpty()) lse[i] = 0;
-            else lse[i] = stk.peek() + 1;
+        for (int i=0; i <= n; i++) {
+            while (!stk.isEmpty() && (i==heights.length || heights[i] < heights[stk.peek()]) ) {
+                int top = stk.pop();
+                
+                int width = 0;
+                if (stk.isEmpty()) width = i;
+                else width = i - stk.peek() - 1;
+                area = Math.max(area, width * heights[top]);
+            }
 
             stk.push(i);
         }
-
-        stk.clear();
-
-        for (int i=n - 1; i >= 0; i--) {
-            while (!stk.isEmpty() && heights[i] <= heights[stk.peek()]) {
-                stk.pop();
-            }
-
-            if (stk.isEmpty()) rse[i] = n - 1;
-            else rse[i] = stk.peek() - 1;
-
-            stk.push(i);
-        }
-
-        int area = Integer.MIN_VALUE;
-        for (int i=0; i < n; i++) {
-            area = Math.max( area, (rse[i] - lse[i] + 1)  * heights[i] );
-        }
-
-        System.out.println(Arrays.toString(lse));
-        System.out.println(Arrays.toString(rse));
-
 
         return area;
+        
     }
 }
