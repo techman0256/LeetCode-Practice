0c560fb7fc448040d8df62719cb1605dc1d3672c
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-05-31 10:50:56 +0530

    Time: 12 ms (91.02%) | Memory: 16 MB (81.50%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    bool DFS(int node, vector<int> &vis, vector<vector<int>> &graph) {
+        cout << node << endl;
+        for (auto neigh : graph[node]) {
+            if (vis[neigh] != 0 && vis[neigh] == vis[node]) {
+                return false;
+            } 
+            if (vis[neigh] == 0) {
+                if (vis[node] == 1) vis[neigh] = 2;
+                else vis[neigh] = 1;
+
+                bool check = DFS(neigh, vis, graph);
+                if (check == false) return false;
+            }
+        }
+
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
 
@@ -7,26 +24,10 @@ public:
 
         for (int i=0; i < V; i++) {
             if (vis[i] == 0) {
-                queue<int> bfs;
-                bfs.emplace(i);
                 vis[i] = 1;
-
-                while ( !bfs.empty() ) {
-                    int node = bfs.front();
-                    bfs.pop();
-
-                    for (auto neigh : graph[node]) {
-                        if (vis[neigh] != 0 && vis[neigh] == vis[node] ) {
-                            return false;
-                        }
-                        if (vis[neigh] == 0) {
-                            if (vis[node] == 1) vis[neigh] = 2;
-                            else vis[neigh] = 1;
-
-                            bfs.emplace(neigh);
-                        }
-                    }
-                }
+               
+                bool twoCol = DFS(i, vis, graph);
+                if (twoCol == false) return false;
             }
         }
         
