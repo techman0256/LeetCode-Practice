1f6c774069c3ff418eff6c1981badde41f74af84
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-06-04 15:39:13 +0530

    Time: 46 ms (60.60%) | Memory: 23.9 MB (65.66%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -5,27 +5,27 @@ public:
         int n = grid.size();
 
         vector<vector<int>> dis(n, vector<int> (n, INT_MAX));
-        queue< pair< pair<int, int>, int> > bfs;
+        queue< pair<int, int> > bfs;
         if (grid[0][0] == 1) return -1;
-        bfs.push({{0, 0}, 1});
+        bfs.push({0, 0});
         dis[0][0] = 1;
 
         int dx[] = { -1, 0, 1, 0, -1, 1, 1, -1 };
         int dy[] = { 0, -1, 0, 1, -1, 1, -1, 1 };
 
         while ( !bfs.empty() ) {
-            int u_x = bfs.front().first.first;
-            int u_y = bfs.front().first.second;
-            int len = bfs.front().second;
+            int u_x = bfs.front().first;
+            int u_y = bfs.front().second;
+            // int len = bfs.front().second;
             bfs.pop();
 
             for (int i=0; i < 8; i++) {
                 int v_x = u_x + dx[i];
                 int v_y = u_y + dy[i];
 
-                if (v_x >= 0 && v_y >= 0 && v_x < n && v_y < n && grid[v_x][v_y] == 0 && dis[v_x][v_y] > len + 1) {
-                    dis[v_x][v_y] = len + 1;
-                    bfs.push({{v_x, v_y}, 1 + len});
+                if (v_x >= 0 && v_y >= 0 && v_x < n && v_y < n && grid[v_x][v_y] == 0 && dis[v_x][v_y] > dis[u_x][u_y] + 1) {
+                    dis[v_x][v_y] = dis[u_x][u_y] + 1;
+                    bfs.push({v_x, v_y});
                 } 
             }
         }
