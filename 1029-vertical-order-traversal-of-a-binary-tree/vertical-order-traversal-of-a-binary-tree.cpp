ed4ec0650a7b8cd91b84f56252813fbf341e0827
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-06-22 15:07:56 +0530

    Time: 3 ms (73.65%) | Memory: 14.8 MB (12.50%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -17,46 +17,39 @@ struct Node{
 
 class Solution {
 public:
-    void giveOrder(int row, int col, TreeNode *root, vector<struct Node> &order) {
-        if (root == NULL) {
-            return;
-        }
-        struct Node curr = {row, col, root->val};
-
-        order.push_back(curr);
-        giveOrder(row + 1, col - 1, root->left, order);
-        giveOrder(row + 1, col + 1, root->right, order);
-
-        return;
-    }
-    static bool compare(struct Node x, struct Node y) {
-        if (x.row == y.row && x.col == y.col) {
-            return x.val < y.val;
-        }
-        if (x.col == y.col) {
-            return x.row < y.row;
-        }
-        return x.col < y.col;
-    }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<struct Node> order;
-        vector<vector<int>> ans;
-        giveOrder(0, 0, root, order);
-
-        sort(order.begin(), order.end(), compare);
-        vector<int> col;
-        int prev_col = INT_MIN;
-        for ( auto node : order) {
-            if (prev_col != node.col) {
-                prev_col = node.col;
-                ans.push_back(col);
-                col.clear();
-            }
-            col.push_back(node.val);
-        }
-        ans.push_back(col);
-        ans.erase(ans.begin());
+        map<int, map<int, multiset<int>>> nodes;
         
+        queue<pair<TreeNode*, pair<int, int>>> todo;
+        todo.push({root, {0, 0}});
+        
+        while ( !todo.empty() ) {
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* temp = p.first;
+            
+            int x = p.second.first;
+            int y = p.second.second;
+            
+            nodes[x][y].insert(temp->val);
+            
+            if (temp->left) {
+                todo.push({ temp->left, {x - 1, y + 1} });
+            }
+            if (temp->right) {
+                todo.push({ temp->right, {x + 1, y + 1} });
+            }
+        }
+        
+        vector<vector<int>> ans;
+        for (auto p : nodes) {
+            vector<int> col;
+            for (auto q : p.second) {
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
+     
         return ans;
     }
 };
