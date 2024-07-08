68217ac167040e2ca89c5e82c059ba902c021412
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-07-08 14:21:36 +0530

    Time: 5 ms (32.92%) | Memory: 8 MB (38.24%) - LeetSync

---
+++ 3230-remove-adjacent-almost-equal-characters/remove-adjacent-almost-equal-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool almostEqual(int a, int b) {
+        return abs(a - b) <= 1;
+    }
+    int removeAlmostEqualCharacters(string word) {
+        int n = word.length();
+        int ops = 0;
+        
+        int i = 0;
+        for (i=1; i < n; i++) {
+            if (almostEqual(word[i], word[i - 1])) {
+                word[i] = '#';
+                ops++;
+            }
+
+        }
+       
+        return ops;
+
+
+        // int cnt = 0 ;
+        // for(int i = 1 ; i< word.size(); i++){
+        //     if(abs(word[i]-word[i-1]) <=1){
+        //         cnt++;
+        //         word[i]=(word[i]-'a'+2);
+        //     }
+        // }
+        // return cnt ; 
+
+
+    }
+};
