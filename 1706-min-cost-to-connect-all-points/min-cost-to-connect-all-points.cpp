8a814439b26726e86c75c27418955d3ce37b3193
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2024-07-30 18:14:00 +0530

    Time: 51 ms (94.50%) | Memory: 17.7 MB (90.00%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<int> vis(n,0) , dis(n,INT_MAX);
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> q;
+
+        q.push({0 , 0});
+        dis[0] = 0;
+        while(!q.empty())
+        {
+            auto[wt , node] = q.top();
+            q.pop();
+            if(vis[node] == 1) continue;
+            vis[node] = 1;
+
+            for(int i=0 ; i<n ; i++)
+            {
+                if(vis[i] == 0 && dis[i] > (abs(points[node][0] - points[i][0])+
+                                            abs(points[node][1] - points[i][1])))
+                {
+                    dis[i] = (abs(points[node][0] - points[i][0])+
+                              abs(points[node][1] - points[i][1]));
+                    q.push({dis[i] , i});
+                }
+            }
+        }
+
+        int res = 0;
+        for(auto i:dis)
+        res+=i;
+
+        return res;
+    }
+};
+
+// class Solution {
+// public:
+//     int minCostConnectPoints(vector<vector<int>>& points) {
+//         int n = points.size();
+//         vector<int> vis(n, 0);
+//         vector<int> dis(n, INT_MAX);
+
+//         priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+//         pq.push( {0, 0} );
+//         dis[0] = 0;
+//         while ( !pq.empty() ) {
+//             int node = pq.top().first;
+//             int wt = pq.top().second;
+//             pq.pop();
+
+//             if (vis[node] == 1) continue;
+//             vis[node] = 1;
+
+//             for (int i=0; i < n; i++) {
+//                 if (vis[i] == 0 && dis[i] > abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1])) {
+//                     dis[i] = abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1]);
+//                     pq.push( {i , dis[i]} );
+//                 }
+//             }
+
+
+
+//         }
+//             int res = 0;
+//             for ( auto i : dis ) {
+//                 res += i;
+//             }
+//         return res;
+//     }
+// };
