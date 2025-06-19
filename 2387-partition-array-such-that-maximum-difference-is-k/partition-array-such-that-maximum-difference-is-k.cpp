ee369f19cdac68ab6ca2a3598fcf8d10088c76b9
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2025-06-19 20:15:23 +0530

    Time: 34 ms (66.39%) | Memory: 86.9 MB (39.38%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -4,10 +4,10 @@ public:
         sort(nums.begin(), nums.end());
 
         int count = 0;
-        int min = nums[0];
+        int min = 0;
         for (int i=0; i < nums.size(); i++) {
-            if (nums[i] - min > k) {
-                min = nums[i];
+            if (nums[i] - nums[min] > k) {
+                min = i;
                 count++;
             }
         }
