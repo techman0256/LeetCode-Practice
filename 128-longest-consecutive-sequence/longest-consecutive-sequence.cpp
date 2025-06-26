a9653e4a03e7d2071f4c31732494602e197d8f5f
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2025-06-26 23:14:24 +0530

    Time: 2274 ms (5.01%) | Memory: 127.2 MB (5.09%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findParent(int node, map<int, int> &parent) {
+        if (parent[node] == node) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node], parent);
+    }
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+
+        map<int, int> parent;
+        map<int, int> size;
+        for (int i=0; i < n; i++) {
+            if (parent.find(nums[i]) != parent.end()) {
+                continue;
+            }
+            parent[nums[i]] = nums[i];
+            size[nums[i]] = 1;
+            int pu = findParent(nums[i], parent);
+            if ( parent.find(nums[i] + 1) != parent.end() ) {
+                int pv = findParent(parent[nums[i] + 1], parent);
+
+                parent[pv] = pu;
+                size[pu] += size[pv];
+                size[pv] = 0;
+            }
+            if ( parent.find(nums[i] - 1) != parent.end() ) {
+                int pv = findParent(parent[nums[i] - 1], parent);
+                parent[pv] = pu;
+                size[pu] += size[pv];
+                size[pv] = 0;
+            }
+        }
+
+        int ans = 0;
+        for (auto pair: size) {
+            cout << pair.first << " and " << pair.second << " ";
+            ans = max(ans, pair.second);
+        }
+        cout << endl;
+
+
+        return ans;
+    }
+};
