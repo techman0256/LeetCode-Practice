58d583d40f3411b27d40aa72d552351d0bf35852
Pranav Patel <101399150+techman0256@users.noreply.github.com>
2025-07-04 16:42:54 +0530

    Time: 420 ms (5.30%) | Memory: 185.7 MB (20.41%) - LeetSync

--- 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
+++ 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -1,58 +1,39 @@
 class Solution {
 public:
-    vector<int> next_greater(vector<int> &nums) {
-        vector<int> nge( nums.size(), nums.size());
+    vector<int> findNGE(vector<int> &nums) {
+        int n = nums.size();
+        vector<int> nge(n, n);
+
         stack<int> stk;
-        for (int i=nums.size() - 1; i >= 0; i--) {
-            while ( !stk.empty() && nums[stk.top()] <= nums[i]) {
+        for (int i=n - 1; i >= 0; i--) {
+            while (!stk.empty() && nums[stk.top()] <= nums[i]) {
                 stk.pop();
-            }
-
-            if ( !stk.empty() ) {
-                nge[i] = stk.top();
             } 
-
+            if (!stk.empty()) nge[i] = stk.top();
+            
             stk.push(i);
-
         }
 
         return nge;
     }
     long long numberOfSubarrays(vector<int>& nums) {
         int n = nums.size();
-        long long ans = 0;
+        long long answer = 0;
+        vector<int> nge = findNGE(nums);
 
-        map<int, vector<int>> occ;
+        map<int, vector<int>> next_occ;
         for (int i=0; i < n; i++) {
-            occ[nums[i]].push_back(i);
+            next_occ[nums[i]].push_back(i);
         }
 
-        for ( auto m : occ ) {
-            for ( auto s : m.second) {
-                cout << s << " ";
-            }
-            cout << endl;
+        for (int i=0; i < n; i++) {
+            int curr = upper_bound(next_occ[nums[i]].begin(), next_occ[nums[i]].end(), i) - next_occ[nums[i]].begin();
+            int last = upper_bound(next_occ[nums[i]].begin(), next_occ[nums[i]].end(), nge[i]) - next_occ[nums[i]].begin();
+
+            answer += (last - curr + 1);
+
         }
 
-        long long cnt = 0;
-        vector<int> nge = next_greater(nums);
-        // for (int i=0; i < n; i++) {
-        //     cout << nge[i] << " ";
-        // }
-        // cout << endl;
-        // auto it = upper_bound(occ[3].begin(), occ[3].end(), 12);
-        // int diff = it - occ[3].begin() - 1;
-        // cout << diff << endl;
-        for (int i=0; i < n; i++) {
-            int ng = nge[i];
-            int curr = upper_bound(occ[nums[i]].begin(), occ[nums[i]].end(), i) - occ[nums[i]].begin() - 1;
-            int last = upper_bound(occ[nums[i]].begin(), occ[nums[i]].end(), ng) - occ[nums[i]].begin() - 1;
-            cnt += last - curr + 1;
-            // cout << last - curr << endl;
-        }    
-
-
-        return cnt;
-
+        return answer;
     }
 };
